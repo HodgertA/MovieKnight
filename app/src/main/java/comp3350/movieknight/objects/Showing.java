d539a145b25fce51c8a4c8e6080ca4cfef4eee1c
yuangBI <biy1@myumanitoba.ca>
2021-06-09 23:41:36 +0800

    finish modify the Showing.java

--- app/src/main/java/comp3350/movieknight/objects/Showing.java
+++ app/src/main/java/comp3350/movieknight/objects/Showing.java
@@ -1,37 +1,37 @@
 package comp3350.movieknight.objects;
 
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 
-import java.util.Date;
 
 public class Showing {
     private String showingID;
     private String movieID;
     private String theatreID;
-    private Date showingDay;
+
+    private Calendar showingDate;
 
     private ArrayList<Boolean> seats;
 
     public Showing(String showingID, String movieID, String theatreID, int year,int month,int date,int hour, int minute) {
         if (showingID != null && movieID != null && theatreID != null && hour >= 0 && hour <= 23 && minute >= 0 && minute <=59) {
+            this.seats = new ArrayList<Boolean>();
             this.showingID = showingID;
             this.movieID = movieID;
             this.theatreID = theatreID;
+            this.showingDate =Calendar.getInstance();
+            this.showingDate.set(year,month,date,hour,minute);
 
-            try{
-            SimpleDateFormat format=new SimpleDateFormat("yyyy-MM-dd HH:mm");
-            this.showingDay=format.parse(year+"-"+month+"-"+date+" "+hour+":"+minute);
-            }catch (ParseException e){
 
-            }
-
-            seats = new ArrayList<Boolean>();
         } else {
             throw new IllegalArgumentException("Invalid showing attributes");
         }
+    }
+
+
+    public Calendar getShowingDate() {
+        return showingDate;
     }
 
     public String getShowingID() { return showingID; }
@@ -46,15 +46,13 @@ public class Showing {
         return theatreID;
     }
 
-    public Date getShowingDay() {
-        return showingDay;
-    }
+
 
     public ArrayList<Boolean> getSeats() { return seats; }
 
     public String toString()
     {
-        return "Showing: "+ showingID + ", Movie: " + movieID + ", Theatre: " + theatreID + ", Showing Time: " + showingDay;
+        return "Showing: "+ showingID + ", Movie: " + movieID + ", Theatre: " + theatreID;
     }
 
     public boolean equals(Object object)
@@ -66,8 +64,7 @@ public class Showing {
         {
             showing = (Showing) object;
             if (((showing.movieID == null && movieID == null) || (showing.movieID != null && showing.movieID.equals(movieID)))
-                && ((showing.theatreID == null  && theatreID == null)  || (showing.theatreID != null  && showing.theatreID.equals(theatreID)))
-                && ((showing.showingDay == null && showingDay == null) || (showing.showingDay != null && showing.showingDay.equals(showingDay))))
+                && ((showing.theatreID == null  && theatreID == null)  || (showing.theatreID != null  && showing.theatreID.equals(theatreID))))
             {
                 result = true;
             }
--- app/src/main/java/comp3350/movieknight/persistence/DatabaseStub.java
+++ app/src/main/java/comp3350/movieknight/persistence/DatabaseStub.java
@@ -1,6 +1,6 @@
 package comp3350.movieknight.persistence;
 
-import java.text.ParseException;
+
 import java.util.ArrayList;
 import java.util.List;
 
