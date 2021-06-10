cae75f879412e5de27529d4991a26587a7f66bd8
brynn <brynn.dagdick@gmail.com>
2021-06-10 13:05:44 -0500

    New Ticket tests

--- app/src/main/java/comp3350/movieknight/objects/Ticket.java
+++ app/src/main/java/comp3350/movieknight/objects/Ticket.java
@@ -8,36 +8,28 @@ public class Ticket {
     private int showingID;
     private int theatreID;
 
-    public Ticket(int ticketID, int userID,int showingID,int theatreID,int seatNum){
-        this.ticketID=ticketID;
-        this.userID=userID;
-        this.showingID=showingID;
-        this.theatreID=theatreID;
-        this.seatNum=seatNum;
+    public Ticket(int ticketID, int userID, int showingID, int theatreID, int seatNum){
+        if (ticketID >= 0 && userID >= 0 && showingID >= 0 && theatreID >= 0 && seatNum> 0) {
+            this.ticketID = ticketID;
+            this.userID = userID;
+            this.showingID = showingID;
+            this.theatreID = theatreID;
+            this.seatNum = seatNum;
+        } else {
+            throw new IllegalArgumentException();
+        }
     }
 
     public Ticket(int ticketID, int showingID, int theatreID, int seatNum){
-        this.ticketID=ticketID;
-        this.userID= -1;
-        this.showingID=showingID;
-        this.theatreID=theatreID;
-        this.seatNum=seatNum;
-    }
-
-    public void setSeatNum(int seatNum) {
-        this.seatNum = seatNum;
-    }
-
-    public void setShowingID(int showingID) {
-        this.showingID = showingID;
-    }
-
-    public void setTheatreID(int theatreID) {
-        this.theatreID = theatreID;
-    }
-
-    public void setUserID(int userID) {
-        this.userID = userID;
+        if (ticketID >= 0 && showingID >= 0 && theatreID >= 0 && seatNum> 0) {
+            this.ticketID = ticketID;
+            this.userID = -1;
+            this.showingID = showingID;
+            this.theatreID = theatreID;
+            this.seatNum = seatNum;
+        } else {
+            throw new IllegalArgumentException();
+        }
     }
 
     public int getSeatNum() {
@@ -63,56 +55,30 @@ public class Ticket {
     @Override
     public String toString() {
         return "Ticket: " +
-                "ticketID=" + ticketID +
-                ", seatNum=" + seatNum +
-                ", userID=" + userID +
-                ", showingID=" + showingID +
-                ", theatreID=" + theatreID ;
+                "ticketID= " + ticketID +
+                ", seatNum= " + seatNum +
+                ", userID= " + userID +
+                ", showingID= " + showingID +
+                ", theatreID= " + theatreID ;
     }
 
     @Override
-    //------------------------------------------
-    //this equals function compare ticketID
-    //------------------------------------------
     public boolean equals(Object o) {
-        boolean result = false;
+
+        boolean result=false;
         Ticket ticket;
 
         if(o instanceof Ticket)
         {
             ticket=(Ticket) o;
-            if((ticket.ticketID == ticketID))
+            if(ticket.userID == userID
+                    && ticket.showingID == showingID
+                    && ticket.seatNum == seatNum)
             {
                 result=true;
             }
         }
 
         return result;
-    }
-
-
-//    @Override
-//    //------------------------------------------
-//    //this equals function compare userID, showingID and seatNum
-//    //------------------------------------------
-//    public boolean equals(Object o) {
-//
-//        boolean result=false;
-//        Ticket ticket;
-//
-//        if(o instanceof Ticket)
-//        {
-//            ticket=(Ticket) o;
-//            if(((ticket.userID==null)&&(userID==null))||(ticket.userID.equals(userID))
-//                    &&((ticket.showingID==null)&&(showingID==null))||(ticket.showingID.equals(showingID))
-//                    &&(ticket.seatNum==seatNum))
-//            {
-//                result=true;
-//            }
-//        }
-//
-//
-//        return result;
-//    }
-
+   }
 }
--- app/src/test/java/comp3350/movieknight/tests/objects/TicketTest.java
+++ app/src/test/java/comp3350/movieknight/tests/objects/TicketTest.java
@@ -3,61 +3,160 @@ package comp3350.movieknight.tests.objects;
 import junit.framework.TestCase;
 
 import comp3350.movieknight.objects.Ticket;
+import comp3350.movieknight.objects.User;
 
 public class TicketTest extends TestCase {
 
     public TicketTest(String arg0) { super(arg0);}
 
-    public void testConstructor(){
-        System.out.println("starting testConstructor for Ticket class\n");
+    public void testTypicalTicket()
+    {
+        System.out.println("Starting Ticket test: testTypicalTicket");
 
-            Ticket ticket1 = new Ticket(1,1,1,1,1);
-            assertNotNull(ticket1);
-            assertEquals(1,ticket1.getTicketID());
-            assertEquals(1,ticket1.getUserID());
-            assertEquals(1,ticket1.getShowingID());
-            assertEquals(1,ticket1.getTheatreID());
-            assertEquals(1,ticket1.getSeatNum());
+        Ticket ticket1 = new Ticket(1, 1, 1, 1, 10);
+        assertNotNull(ticket1);
+        assertEquals(1, ticket1.getTicketID());
+        assertEquals(1, ticket1.getUserID());
+        assertEquals(1, ticket1.getShowingID());
+        assertEquals(1, ticket1.getTheatreID());
+        assertEquals(10, ticket1.getSeatNum());
+        assertEquals("Ticket: ticketID= 1, seatNum= 10, userID= 1, showingID= 1, theatreID= 1", ticket1.toString());
+        assertTrue(ticket1.equals(ticket1));
 
-            Ticket ticket2 = new Ticket(2,2,2,2);
-            assertNotNull(ticket2);
-            assertEquals(2,ticket2.getTicketID());
-            assertEquals(2,ticket2.getShowingID());
-            assertEquals(2,ticket2.getTheatreID());
-            assertEquals(2,ticket2.getSeatNum());
+        Ticket ticket2 = new Ticket(1, 1, 1, 10);
+        assertNotNull(ticket2);
+        assertEquals(1, ticket2.getTicketID());
+        assertEquals(-1, ticket2.getUserID());
+        assertEquals(1, ticket2.getShowingID());
+        assertEquals(1, ticket2.getTheatreID());
+        assertEquals(10, ticket2.getSeatNum());
+        assertEquals("Ticket: ticketID= 1, seatNum= 10, userID= -1, showingID= 1, theatreID= 1", ticket2.toString());
+        assertTrue(ticket2.equals(ticket2));
 
-        System.out.println("Finished testConstructor for Ticket class\n\n");
+        System.out.println("Finished Ticket test: testTypicalTicket");
     }
 
-    public void testToString(){
+    public void testTwoTickets()
+    {
+        System.out.println("Starting Ticket test: testTwoTickets");
 
-        System.out.println("starting testToString for Ticket class\n");
+        Ticket ticket1 = new Ticket(1, 1, 1, 1, 10);
+        Ticket ticket2 = new Ticket(2, 2, 2, 2, 20);
+        assertNotNull(ticket1);
+        assertNotNull(ticket2);
+        assertEquals(1, ticket1.getTicketID());
+        assertEquals(2, ticket2.getTicketID());
+        assertEquals(1, ticket1.getUserID());
+        assertEquals(2, ticket2.getUserID());
+        assertEquals(1, ticket1.getShowingID());
+        assertEquals(2, ticket2.getShowingID());
+        assertEquals(1, ticket1.getTheatreID());
+        assertEquals(2, ticket2.getTheatreID());
+        assertEquals(10, ticket1.getSeatNum());
+        assertEquals(20, ticket2.getSeatNum());
+        assertEquals("Ticket: ticketID= 1, seatNum= 10, userID= 1, showingID= 1, theatreID= 1", ticket1.toString());
+        assertEquals("Ticket: ticketID= 2, seatNum= 20, userID= 2, showingID= 2, theatreID= 2", ticket2.toString());
+        assertFalse(ticket1.equals(ticket2));
 
-            Ticket ticket1 = new Ticket(1,1,1,1,1);
-            assertEquals("Ticket: ticketID=1, seatNum=1, userID=1, showingID=1, theatreID=1",ticket1.toString());
+        Ticket ticket3 = new Ticket(1, 1, 1, 10);
+        Ticket ticket4 = new Ticket(2, 2, 2, 20);
+        assertNotNull(ticket3);
+        assertNotNull(ticket4);
+        assertEquals(1, ticket3.getTicketID());
+        assertEquals(2, ticket4.getTicketID());
+        assertEquals(-1, ticket3.getUserID());
+        assertEquals(-1, ticket4.getUserID());
+        assertEquals(1, ticket3.getShowingID());
+        assertEquals(2, ticket4.getShowingID());
+        assertEquals(1, ticket3.getTheatreID());
+        assertEquals(2, ticket4.getTheatreID());
+        assertEquals(10, ticket3.getSeatNum());
+        assertEquals(20, ticket4.getSeatNum());
+        assertEquals("Ticket: ticketID= 1, seatNum= 10, userID= -1, showingID= 1, theatreID= 1", ticket3.toString());
+        assertEquals("Ticket: ticketID= 2, seatNum= 20, userID= -1, showingID= 2, theatreID= 2", ticket4.toString());
+        assertFalse(ticket3.equals(ticket4));
 
-        System.out.println("Finished testToString for Ticket class\n\n");
-
+        System.out.println("Finished Ticket test: testTwoTickets");
     }
 
-    public void testEquals(){
-        System.out.println("starting testEquals for Ticket class\n");
-            Ticket ticket1 = new Ticket(1,1,1,1,1);
-            Ticket ticket1Copy = new Ticket(1,1,1,1,1);
-            Ticket ticket2 = new Ticket(2,2,2,2);
-            Ticket ticket3 = new Ticket(1,2,2,2);
+    public void testEdgeCases()
+    {
+        System.out.println("Starting Ticket test: testEdgeCases");
 
-            boolean temp;
+        Ticket ticket1 = new Ticket(0, 0, 0, 0, 1);
+        assertNotNull(ticket1);
+        assertEquals(0, ticket1.getTicketID());
+        assertEquals(0, ticket1.getUserID());
+        assertEquals(0, ticket1.getShowingID());
+        assertEquals(0, ticket1.getTheatreID());
+        assertEquals(1, ticket1.getSeatNum());
+        assertEquals("Ticket: ticketID= 0, seatNum= 1, userID= 0, showingID= 0, theatreID= 0", ticket1.toString());
+        assertTrue(ticket1.equals(ticket1));
 
-            temp=ticket1Copy.equals(ticket1);
-            assertTrue(temp);
+        Ticket ticket2 = new Ticket(0, 0, 0, 1);
+        assertNotNull(ticket2);
+        assertEquals(0, ticket2.getTicketID());
+        assertEquals(-1, ticket2.getUserID());
+        assertEquals(0, ticket2.getShowingID());
+        assertEquals(0, ticket2.getTheatreID());
+        assertEquals(1, ticket2.getSeatNum());
+        assertEquals("Ticket: ticketID= 0, seatNum= 1, userID= -1, showingID= 0, theatreID= 0", ticket2.toString());
+        assertTrue(ticket2.equals(ticket2));
 
-            temp=ticket1.equals(ticket2);
-            assertFalse(temp);
+        System.out.println("Finished Ticket test: testEdgeCases");
+    }
 
-            temp=ticket1.equals(ticket3);
-            assertTrue(temp);
+    public void testInvalidValues()
+    {
+        System.out.println("Starting Ticket test: testInvalidValues");
 
-        System.out.println("Finished testEquals for Ticket class\n");
+        Ticket ticket;
+
+        try {
+            ticket = new Ticket(-1, 1,1,1,1);
+            fail("Expected an IllegalArguementException");
+        } catch (IllegalArgumentException ex) {}
+
+        try {
+            ticket = new Ticket(1, -1,1,1,1);
+            fail("Expected an IllegalArguementException");
+        } catch (IllegalArgumentException ex) {}
+
+        try {
+            ticket = new Ticket(1, 1,-1,1,1);
+            fail("Expected an IllegalArguementException");
+        } catch (IllegalArgumentException ex) {}
+
+        try {
+            ticket = new Ticket(1, 1,1,-1,1);
+            fail("Expected an IllegalArguementException");
+        } catch (IllegalArgumentException ex) {}
+
+        try {
+            ticket = new Ticket(1, 1,1,1,0);
+            fail("Expected an IllegalArguementException");
+        } catch (IllegalArgumentException ex) {}
+
+        try {
+            ticket = new Ticket(-1,1,1,1);
+            fail("Expected an IllegalArguementException");
+        } catch (IllegalArgumentException ex) {}
+
+        try {
+            ticket = new Ticket(1,-1,1,1);
+            fail("Expected an IllegalArguementException");
+        } catch (IllegalArgumentException ex) {}
+
+        try {
+            ticket = new Ticket(1,1,-1,1);
+            fail("Expected an IllegalArguementException");
+        } catch (IllegalArgumentException ex) {}
+
+        try {
+            ticket = new Ticket(1,1,1,0);
+            fail("Expected an IllegalArguementException");
+        } catch (IllegalArgumentException ex) {}
+
+        System.out.println("Finished Ticket test: testInvalidValues");
     }
 }
