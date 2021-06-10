24923be11882bf8339724fe4e02337b0a3e299c9
yuangBI <biy1@myumanitoba.ca>
2021-06-10 18:37:47 +0800

    create empty filterListTest

---
+++ app/src/test/java/comp3350/movieknight/tests/business/FilterListTest.java
@@ -0,0 +1,16 @@
+package comp3350.movieknight.tests.business;
+
+import junit.framework.TestCase;
+
+public class FilterListTest extends TestCase {
+    public FilterListTest(String arg0)
+    {
+        super(arg0);
+    }
+
+    public void testFilterShowingsByDate(){
+
+
+    }
+
+}
