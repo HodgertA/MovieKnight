7977b8e22f280057309928bb64a5aa1366d3393a
yuangBI <biy1@myumanitoba.ca>
2021-06-13 17:17:46 +0800

    fix some null parameter problem

--- app/src/main/java/comp3350/movieknight/business/FilterList.java
+++ app/src/main/java/comp3350/movieknight/business/FilterList.java
@@ -34,15 +34,18 @@ public class FilterList {
 
     public static String filterShowingsByDate(ArrayList<Showing> allShowing){
 
-        Calendar toDay=Calendar.getInstance();
-        Iterator<Showing> iterator=allShowing.iterator();
-        while (iterator.hasNext()){
-            Showing sh=iterator.next();
+        if(allShowing!=null) {
+            Calendar toDay = Calendar.getInstance();
+            Iterator<Showing> iterator = allShowing.iterator();
+            while (iterator.hasNext()) {
+                Showing sh = iterator.next();
 
-            if(     sh.getShowingDate().get(Calendar.YEAR)!=toDay.get(Calendar.YEAR)||
-                    sh.getShowingDate().get(Calendar.MONTH)!=toDay.get(Calendar.MONTH)||
-                    sh.getShowingDate().get(Calendar.DATE)!=toDay.get(Calendar.DATE)){
-                iterator.remove();
+                if (    sh==null||
+                        sh.getShowingDate().get(Calendar.YEAR) != toDay.get(Calendar.YEAR) ||
+                        sh.getShowingDate().get(Calendar.MONTH) != toDay.get(Calendar.MONTH) ||
+                        sh.getShowingDate().get(Calendar.DATE) != toDay.get(Calendar.DATE)) {
+                    iterator.remove();
+                }
             }
         }
 
