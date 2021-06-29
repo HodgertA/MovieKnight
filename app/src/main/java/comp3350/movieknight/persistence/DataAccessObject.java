6a37f6fa130d6d22fefbf82f6d1b33a91c0f5a48
brynn <brynn.dagdick@gmail.com>
2021-06-29 13:17:24 -0500

    Finished Movie and Theatre methods

--- app/src/main/java/comp3350/movieknight/persistence/DataAccessObject.java
+++ app/src/main/java/comp3350/movieknight/persistence/DataAccessObject.java
@@ -1,10 +1,12 @@
 package comp3350.movieknight.persistence;
 
+import java.sql.SQLWarning;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 import comp3350.movieknight.objects.Movie;
@@ -15,9 +17,9 @@ import comp3350.movieknight.objects.User;
 
 public class DataAccessObject implements DataAccess {
 
-    private Statement st1, st2, st3;
+    private Statement st1, st2, st3, st4, st5;
     private Connection c1;
-    private ResultSet rs2, rs3, rs4, rs5;
+    private ResultSet rs2, rs3, rs4, rs5, rs6, rs7, rs8, rs9;
 
     private String dbName;
     private String dbType;
@@ -73,47 +75,204 @@ public class DataAccessObject implements DataAccess {
 
     @Override
     public String getAllMovies(List<Movie> movieResult) {
-        return null;
+        Movie movie;
+        int movieID, poster, runtime;
+        String description, title;
+        long lastShowDate;
+
+        result = null;
+        try {
+            cmdString = "Select * from Movies";
+            rs2 = st1.executeQuery(cmdString);
+        } catch (Exception e) {
+            processSQLError(e);
+        }
+
+        try {
+            while (rs2.next())
+            {
+                movieID = rs2.getInt("MovieID");
+                description = rs2.getString("Description");
+                title = rs2.getString("Title");
+                poster = rs2.getInt("Poster");
+                runtime = rs2.getInt("Runtime");
+                lastShowDate = rs2.getLong("LastShowDate");
+
+                Calendar calendar = Calendar.getInstance();
+                calendar.setTimeInMillis(lastShowDate);
+
+                movie = new Movie(movieID, description, title, poster, runtime, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DATE));
+                movieResult.add(movie);
+            }
+        } catch (Exception e) {
+            result = processSQLError(e);
+        }
+
+        return result;
     }
 
     @Override
     public String insertMovie(Movie movie) {
-        return null;
+        String values;
+
+        result = null;
+        try {
+            values = movie.getMovieID() + ", '"
+                    + movie.getDescription()
+                    + "', '" + movie.getTitle()
+                    + "', " + movie.getPoster()
+                    + ", " + movie.getRuntime()
+                    + ", " + movie.getLastShowDate().getTimeInMillis();
+
+            cmdString = "Insert into Movies " +" Values(" + values +")";
+            updateCount = st1.executeUpdate(cmdString);
+            result = checkWarning(st1, updateCount);
+        } catch (Exception e) {
+            result = processSQLError(e);
+        }
+        return result;
     }
 
     @Override
     public String updateMovie(Movie movie) {
-        return null;
+        String values;
+        String where;
+
+        result = null;
+        try {
+            values = "Description='" + movie.getDescription()
+                    + "', Title='" + movie.getTitle()
+                    + "', Poster=" + movie.getPoster()
+                    + ", Runtime=" + movie.getRuntime()
+                    + ", LastShowDate=" + movie.getLastShowDate().getTimeInMillis();
+            where = "where MovieID=" + movie.getMovieID();
+
+            cmdString = "Update Movies " + " Set " +values+ " "+where;
+            updateCount = st1.executeUpdate(cmdString);
+            result = checkWarning(st1, updateCount);
+        } catch (Exception e) {
+            result = processSQLError(e);
+        }
+        return result;
     }
 
     @Override
     public String deleteMovie(Movie movie) {
-        return null;
+        result = null;
+        try {
+            cmdString = "Delete from Movies where MovieID="+movie.getMovieID();
+            updateCount = st1.executeUpdate(cmdString);
+            result = checkWarning(st1, updateCount);
+        } catch (Exception e){
+            result = processSQLError(e);
+        }
+        return result;
     }
 
     @Override
     public String getAllTheatres(List<Theatre> theatreResult) {
-        return null;
+        Theatre theatre;
+        int theatreID, numSeats;
+
+        result = null;
+        try {
+            cmdString = "Select * from Theatres";
+            rs3 = st2.executeQuery(cmdString);
+        } catch (Exception e) {
+            processSQLError(e);
+        }
+
+        try {
+            while (rs4.next())
+            {
+                theatreID = rs3.getInt("TheatreID");
+                numSeats = rs3.getInt("NumSeats");
+
+                theatre = new Theatre(theatreID, numSeats);
+                theatreResult.add(theatre);
+            }
+        } catch (Exception e) {
+            result = processSQLError(e);
+        }
+
+        return result;
     }
 
     @Override
     public Theatre getTheatre(Theatre theatre) {
-        return null;
+        int theatreID, numSeats;
+
+        Theatre result = null;
+        try {
+            cmdString = "Select * from Theatres where TheatreID=" + theatre.getTheatreNumber();
+            rs4 = st2.executeQuery(cmdString);
+        } catch (Exception e) {
+            processSQLError(e);
+        }
+
+        try {
+            while (rs5.next())
+            {
+                theatreID = rs4.getInt("TheatreID");
+                numSeats = rs4.getInt("NumSeats");
+
+                result = new Theatre(theatreID, numSeats);
+            }
+        } catch (Exception e) {
+            processSQLError(e);
+        }
+
+        return result;
     }
 
     @Override
     public String insertTheatre(Theatre theatre) {
-        return null;
+        String values;
+
+        result = null;
+        try {
+            values = theatre.getTheatreNumber() + ", '"
+                    + theatre.getNumSeats();
+
+            cmdString = "Insert into Theatres " +" Values(" + values +")";
+            updateCount = st2.executeUpdate(cmdString);
+            result = checkWarning(st2, updateCount);
+        } catch (Exception e) {
+            result = processSQLError(e);
+        }
+        return result;
     }
 
     @Override
     public String updateTheatre(Theatre theatre) {
-        return null;
+        String values;
+        String where;
+
+        result = null;
+        try {
+            values = "NumSeats= " + theatre.getNumSeats();
+            where = "where TheatreID=" + theatre.getTheatreNumber();
+
+            cmdString = "Update Theatres " + " Set " +values+ " "+where;
+            updateCount = st2.executeUpdate(cmdString);
+            result = checkWarning(st2, updateCount);
+        } catch (Exception e) {
+            result = processSQLError(e);
+        }
+        return result;
     }
 
     @Override
     public String deleteTheatre(Theatre theatre) {
-        return null;
+        result = null;
+        try {
+            cmdString = "Delete from Theatres where TheatreID="+theatre.getTheatreNumber();
+            updateCount = st2.executeUpdate(cmdString);
+            result = checkWarning(st2, updateCount);
+        } catch (Exception e){
+            result = processSQLError(e);
+        }
+        return result;
     }
 
     @Override
@@ -199,6 +358,30 @@ public class DataAccessObject implements DataAccess {
     @Override
     public String deleteTicket(Ticket ticket) {
         return null;
+    }
+
+    public String checkWarning(Statement st, int updateCount)
+    {
+        String result;
+
+        result = null;
+        try
+        {
+            SQLWarning warning = st.getWarnings();
+            if (warning != null)
+            {
+                result = warning.getMessage();
+            }
+        }
+        catch (Exception e)
+        {
+            result = processSQLError(e);
+        }
+        if (updateCount != 1)
+        {
+            result = "Tuple not inserted correctly.";
+        }
+        return result;
     }
 
     public String processSQLError(Exception e)
