aed0ef20273c84a92522f5ec255bcce3d79d39c1
yuangBI <biy1@myumanitoba.ca>
2021-07-09 01:32:37 +0800

    add getShowingByID to AccessShowing

--- app/src/main/java/comp3350/movieknight/business/AccessShowing.java
+++ app/src/main/java/comp3350/movieknight/business/AccessShowing.java
@@ -2,6 +2,7 @@ package comp3350.movieknight.business;
 
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Iterator;
 
 import comp3350.movieknight.application.Main;
 import comp3350.movieknight.application.Services;
@@ -22,5 +23,26 @@ public class AccessShowing {
     public String getShowingForMovieByDate(ArrayList<Showing> showingList, int movieID, Calendar date){
         dataAccess.getMovieShowings(showingList ,movieID);
         return FilterList.filterShowingsBySelectDate(showingList,date);
+    }
+    public ArrayList<Showing> getShowingByID(int ShowingID){
+        ArrayList<Showing> showings=new ArrayList<>();
+        dataAccess.getAllShowings(showings);
+
+        if(showings != null) {
+
+            Iterator<Showing> iterator = showings.iterator();
+
+            while (iterator.hasNext()) {
+
+                Showing sh = iterator.next();
+
+                if (sh.getShowingID()!=ShowingID) {
+                    iterator.remove();
+                }
+            }
+        }
+
+
+        return showings;
     }
 }
