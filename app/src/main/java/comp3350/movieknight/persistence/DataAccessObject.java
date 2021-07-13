bd54a35522763150ac75bb3e2d0a860e108d9240
brynn <brynn.dagdick@gmail.com>
2021-07-12 19:55:54 -0500

    Fixed result variables in DataAccessObject

--- app/src/main/java/comp3350/movieknight/persistence/DataAccessObject.java
+++ app/src/main/java/comp3350/movieknight/persistence/DataAccessObject.java
@@ -18,7 +18,7 @@ public class DataAccessObject implements DataAccess {
 
     private Statement st1, st2, st3, st4, st5;
     private Connection c1;
-    private ResultSet rs2, rs3, rs4, rs5, rs6, rs7, rs8, rs9, rs10;
+    private ResultSet rs2, rs3, rs4, rs5, rs6, rs7, rs8, rs9, rs10, rs11, rs12, rs13;
 
     private String dbName;
     private String dbType;
@@ -119,20 +119,20 @@ public class DataAccessObject implements DataAccess {
         Movie result = null;
         try {
             cmdString = "Select * from Movies where MovieID=" + movie.getMovieID();
-            rs2 = st1.executeQuery(cmdString);
+            rs3 = st1.executeQuery(cmdString);
         } catch (Exception e) {
             processSQLError(e);
         }
 
         try {
-            while (rs2.next())
+            while (rs3.next())
             {
-                movieID = rs2.getInt("MovieID");
-                description = rs2.getString("Description");
-                title = rs2.getString("Title");
-                poster = rs2.getString("Poster");
-                runtime = rs2.getInt("Runtime");
-                lastShowDate = rs2.getLong("LastShowDate");
+                movieID = rs3.getInt("MovieID");
+                description = rs3.getString("Description");
+                title = rs3.getString("Title");
+                poster = rs3.getString("Poster");
+                runtime = rs3.getInt("Runtime");
+                lastShowDate = rs3.getLong("LastShowDate");
 
                 Calendar calendar = Calendar.getInstance();
                 calendar.setTimeInMillis(lastShowDate);
@@ -213,16 +213,16 @@ public class DataAccessObject implements DataAccess {
         result = null;
         try {
             cmdString = "Select * from Theatres";
-            rs3 = st2.executeQuery(cmdString);
+            rs4 = st2.executeQuery(cmdString);
         } catch (Exception e) {
             processSQLError(e);
         }
 
         try {
-            while (rs3.next())
+            while (rs4.next())
             {
-                theatreID = rs3.getInt("TheatreID");
-                numSeats = rs3.getInt("NumSeats");
+                theatreID = rs4.getInt("TheatreID");
+                numSeats = rs4.getInt("NumSeats");
 
                 theatre = new Theatre(theatreID, numSeats);
                 theatreResult.add(theatre);
@@ -241,7 +241,7 @@ public class DataAccessObject implements DataAccess {
         Theatre result = null;
         try {
             cmdString = "Select * from Theatres where TheatreID=" + theatre.getTheatreID();
-            rs4 = st2.executeQuery(cmdString);
+            rs5 = st2.executeQuery(cmdString);
         } catch (Exception e) {
             processSQLError(e);
         }
@@ -249,8 +249,8 @@ public class DataAccessObject implements DataAccess {
         try {
             while (rs5.next())
             {
-                theatreID = rs4.getInt("TheatreID");
-                numSeats = rs4.getInt("NumSeats");
+                theatreID = rs5.getInt("TheatreID");
+                numSeats = rs5.getInt("NumSeats");
 
                 result = new Theatre(theatreID, numSeats);
             }
@@ -322,20 +322,20 @@ public class DataAccessObject implements DataAccess {
         result = null;
         try {
             cmdString = "Select * from Showings";
-            rs4 = st3.executeQuery(cmdString);
+            rs6 = st3.executeQuery(cmdString);
         } catch (Exception e) {
             processSQLError(e);
         }
 
         try {
-            while (rs4.next())
+            while (rs6.next())
             {
-                showingID = rs4.getInt("ShowingID");
-                movieID = rs4.getInt("MovieID");
-                theatreID = rs4.getInt("TheatreID");
-                showingDate = rs4.getLong("ShowingDate");
-                showingTime = rs4.getDouble("ShowingTime");
-                seats = rs4.getInt("Seats");
+                showingID = rs6.getInt("ShowingID");
+                movieID = rs6.getInt("MovieID");
+                theatreID = rs6.getInt("TheatreID");
+                showingDate = rs6.getLong("ShowingDate");
+                showingTime = rs6.getDouble("ShowingTime");
+                seats = rs6.getInt("Seats");
 
                 Calendar calendar = Calendar.getInstance();
                 calendar.setTimeInMillis(showingDate);
@@ -362,19 +362,19 @@ public class DataAccessObject implements DataAccess {
 
         try{
             cmdString="Select * from Showings where ShowingID="+showing.getShowingID();
-            rs4=st3.executeQuery(cmdString);
+            rs7 = st3.executeQuery(cmdString);
         }catch (Exception e){
             processSQLError(e);
         }
 
         try{
-            while (rs4.next()){
-                showingID = rs4.getInt("ShowingID");
-                movieID = rs4.getInt("MovieID");
-                theatreID = rs4.getInt("TheatreID");
-                showingDate = rs4.getLong("ShowingDate");
-                showingTime = rs4.getDouble("ShowingTime");
-                seats = rs4.getInt("Seats");
+            while (rs7.next()){
+                showingID = rs7.getInt("ShowingID");
+                movieID = rs7.getInt("MovieID");
+                theatreID = rs7.getInt("TheatreID");
+                showingDate = rs7.getLong("ShowingDate");
+                showingTime = rs7.getDouble("ShowingTime");
+                seats = rs7.getInt("Seats");
 
                 Calendar calendar = Calendar.getInstance();
                 calendar.setTimeInMillis(showingDate);
@@ -382,14 +382,12 @@ public class DataAccessObject implements DataAccess {
                 int hour = (int) Math.floor(showingTime);
                 int minute = (int) showingTime - hour;
 
-                result =new Showing(showingID, movieID, theatreID, seats, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH)+1, calendar.get(Calendar.DATE),hour,minute);
+                result = new Showing(showingID, movieID, theatreID, seats, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH)+1, calendar.get(Calendar.DATE),hour,minute);
             }
         }catch (Exception e){
             processSQLError(e);
         }
-
         return result;
-
     }
 
     @Override
@@ -402,21 +400,21 @@ public class DataAccessObject implements DataAccess {
 
         try {
             cmdString = "Select * from Showings where MovieID=" + newMovieID;
-            rs5 = st3.executeQuery(cmdString);
+            rs8 = st3.executeQuery(cmdString);
 
         } catch (Exception e) {
             processSQLError(e);
         }
 
         try {
-            while (rs5.next())
+            while (rs8.next())
             {
-                showingID = rs5.getInt("ShowingID");
-                movieID = rs5.getInt("MovieID");
-                theatreID = rs5.getInt("TheatreID");
-                showingDate = rs5.getLong("ShowingDate");
-                showingTime = rs5.getDouble("ShowingTime");
-                seats = rs5.getInt("Seats");
+                showingID = rs8.getInt("ShowingID");
+                movieID = rs8.getInt("MovieID");
+                theatreID = rs8.getInt("TheatreID");
+                showingDate = rs8.getLong("ShowingDate");
+                showingTime = rs8.getDouble("ShowingTime");
+                seats = rs8.getInt("Seats");
 
                 System.out.println(showingID + ", " + movieID + ", " + theatreID + ", " + showingDate + ", " + showingTime + ", " + seats);
 
@@ -444,21 +442,21 @@ public class DataAccessObject implements DataAccess {
 
         try {
             cmdString = "Select * from Showings where TheatreID=" + newTheatreID;
-            rs6 = st3.executeQuery(cmdString);
+            rs9 = st3.executeQuery(cmdString);
 
         } catch (Exception e) {
             processSQLError(e);
         }
 
         try {
-            while (rs6.next())
+            while (rs9.next())
             {
-                showingID = rs6.getInt("ShowingID");
-                movieID = rs6.getInt("MovieID");
-                theatreID = rs6.getInt("TheatreID");
-                showingDate = rs6.getLong("ShowingDate");
-                showingTime = rs6.getDouble("ShowingTime");
-                seats = rs6.getInt("Seats");
+                showingID = rs9.getInt("ShowingID");
+                movieID = rs9.getInt("MovieID");
+                theatreID = rs9.getInt("TheatreID");
+                showingDate = rs9.getLong("ShowingDate");
+                showingTime = rs9.getDouble("ShowingTime");
+                seats = rs9.getInt("Seats");
 
                 Calendar calendar = Calendar.getInstance();
                 calendar.setTimeInMillis(showingDate);
@@ -562,16 +560,16 @@ public class DataAccessObject implements DataAccess {
         result = null;
         try {
             cmdString = "Select * from Users";
-            rs7 = st4.executeQuery(cmdString);
+            rs10 = st4.executeQuery(cmdString);
         } catch (Exception e) {
             processSQLError(e);
         }
 
         try {
-            while (rs7.next())
+            while (rs10.next())
             {
-                userID = rs7.getInt("UserID");
-                username = rs7.getString("Username");
+                userID = rs10.getInt("UserID");
+                username = rs10.getString("Username");
 
                 user = new User(userID, username);
                 userResult.add(user);
@@ -643,17 +641,17 @@ public class DataAccessObject implements DataAccess {
         result = null;
         try {
             cmdString = "Select * from Tickets";
-            rs8 = st5.executeQuery(cmdString);
+            rs11 = st5.executeQuery(cmdString);
         } catch (Exception e) {
             processSQLError(e);
         }
 
         try {
-            while (rs8.next())
+            while (rs11.next())
             {
-                userID = rs8.getInt("UserID");
-                showingID = rs8.getInt("ShowingID");
-                seatNum = rs8.getInt("SeatNum");
+                userID = rs11.getInt("UserID");
+                showingID = rs11.getInt("ShowingID");
+                seatNum = rs11.getInt("SeatNum");
 
                 ticket = new Ticket(userID, showingID, seatNum);
                 ticketResult.add(ticket);
@@ -673,17 +671,17 @@ public class DataAccessObject implements DataAccess {
         result = null;
         try {
             cmdString = "Select * from Tickets where ShowingID= " + newShowingID;
-            rs9 = st5.executeQuery(cmdString);
+            rs12 = st5.executeQuery(cmdString);
         } catch (Exception e) {
             processSQLError(e);
         }
 
         try {
-            while (rs9.next())
+            while (rs12.next())
             {
-                userID = rs9.getInt("UserID");
-                showingID = rs9.getInt("ShowingID");
-                seatNum = rs9.getInt("SeatNum");
+                userID = rs12.getInt("UserID");
+                showingID = rs12.getInt("ShowingID");
+                seatNum = rs12.getInt("SeatNum");
 
                 ticket = new Ticket(userID, showingID, seatNum);
                 ticketResult.add(ticket);
@@ -703,17 +701,17 @@ public class DataAccessObject implements DataAccess {
         result = null;
         try {
             cmdString = "Select * from Tickets where UserID= " + newUserID;
-            rs10 = st5.executeQuery(cmdString);
+            rs13 = st5.executeQuery(cmdString);
         } catch (Exception e) {
             processSQLError(e);
         }
 
         try {
-            while (rs10.next())
+            while (rs13.next())
             {
-                userID = rs10.getInt("UserID");
-                showingID = rs10.getInt("ShowingID");
-                seatNum = rs10.getInt("SeatNum");
+                userID = rs13.getInt("UserID");
+                showingID = rs13.getInt("ShowingID");
+                seatNum = rs13.getInt("SeatNum");
 
                 ticket = new Ticket(userID, showingID, seatNum);
                 ticketResult.add(ticket);
