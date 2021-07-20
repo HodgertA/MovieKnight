a09d590a5eb671c8a83abbe0b4b0116498a43b49
brynn <brynn.dagdick@gmail.com>
2021-07-20 15:28:52 -0500

    Created an AccessUser Class

---
+++ app/src/main/java/comp3350/movieknight/business/AccessUser.java
@@ -0,0 +1,36 @@
+package comp3350.movieknight.business;
+
+import java.util.ArrayList;
+
+import comp3350.movieknight.application.Main;
+import comp3350.movieknight.application.Services;
+import comp3350.movieknight.objects.User;
+import comp3350.movieknight.persistence.DataAccess;
+
+public class AccessUser {
+    private DataAccess dataAccess;
+
+    public AccessUser()
+    {
+        dataAccess = Services.getDataAccess(Main.dbName);
+    }
+
+    public String getAllUsers(ArrayList<User> users) {
+        return dataAccess.getAllUsers(users);
+    }
+
+    public String insertUser(User user)
+    {
+        return dataAccess.insertUser(user);
+    }
+
+    public String updateUser(User user)
+    {
+        return dataAccess.updateUser(user);
+    }
+
+    public String deleteUser(User user)
+    {
+        return dataAccess.deleteUser(user);
+    }
+}
