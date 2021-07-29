f2ec8367e0e23204dd42421d79601aee3605428f
Aidan Andrew-Hodgert <hodgerta@gmail.com>
2021-07-29 09:18:14 -0700

    Purchase tickets acceptance test

--- app/src/androidTest/java/comp3350/movieknight/tests/RunAcceptanceTests.java
+++ app/src/androidTest/java/comp3350/movieknight/tests/RunAcceptanceTests.java
@@ -3,12 +3,13 @@ package comp3350.movieknight.tests;
 import org.junit.runners.Suite;
 import org.junit.runner.RunWith;
 
+import comp3350.movieknight.tests.acceptance.PurchaseTicketsAcceptanceTest;
 import comp3350.movieknight.tests.acceptance.ReserveSeatsAcceptanceTest;
 import comp3350.movieknight.tests.acceptance.ViewTicketAcceptanceTest;
 import comp3350.movieknight.tests.acceptance.ViewMoviesAcceptanceTest;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ViewMoviesAcceptanceTest.class, ViewTicketAcceptanceTest.class, ReserveSeatsAcceptanceTest.class})
+@Suite.SuiteClasses({ViewMoviesAcceptanceTest.class, ViewTicketAcceptanceTest.class, ReserveSeatsAcceptanceTest.class, PurchaseTicketsAcceptanceTest.class})
 public class RunAcceptanceTests
 {
     public RunAcceptanceTests()
