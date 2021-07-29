b7a7fefc6dff99a3af909086e058a4a190226e06
brynn <brynn.dagdick@gmail.com>
2021-07-29 10:21:24 -0500

    Created the SendTicketAcceptanceTest

--- app/src/androidTest/java/comp3350/movieknight/tests/RunAcceptanceTests.java
+++ app/src/androidTest/java/comp3350/movieknight/tests/RunAcceptanceTests.java
@@ -3,11 +3,13 @@ package comp3350.movieknight.tests;
 import org.junit.runners.Suite;
 import org.junit.runner.RunWith;
 
+import comp3350.movieknight.tests.acceptance.SendTicketAcceptanceTest;
+import comp3350.movieknight.tests.acceptance.ProfileAcceptanceTest;
 import comp3350.movieknight.tests.acceptance.ViewTicketAcceptanceTest;
 import comp3350.movieknight.tests.acceptance.viewMoviesAcceptanceTest;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({viewMoviesAcceptanceTest.class, ViewTicketAcceptanceTest.class})
+@Suite.SuiteClasses({viewMoviesAcceptanceTest.class, ViewTicketAcceptanceTest.class, ProfileAcceptanceTest.class, SendTicketAcceptanceTest.class})
 public class RunAcceptanceTests
 {
     public RunAcceptanceTests()
